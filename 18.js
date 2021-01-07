63b9a0415b1cc13aed803dc310a1d341958fc0f7
evelyn martins <maevelynmartins@gmail.com>
2021-01-06 23:41:37 -0300

    final

--- 18.js
+++ 18.js
@@ -8,3 +8,14 @@ Depois disso calcule e mostre no console:
 a) O valor dos 10% (cota do garçom)
 b) O valor final da conta (consumo + 10%)
 c) O valor que cada cliente deve pagar */
+
+valor= Number(prompt("Digite o valor total da conta:"))
+pagantes= Number(prompt("Digite o número de clientes:"))
+
+cota= 0.1*valor
+total=1.1*valor
+rateio= total/pagantes
+
+alert("a) O valor dos 10% R$ "+cota)
+alert("b) O valor total R$"+total)
+alert("c) O valor que cada pagante:R$"+rateio)
