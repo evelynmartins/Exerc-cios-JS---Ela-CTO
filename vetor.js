c16a919936041473c38b0d730a96958a58b4e130
evelyn martins <maevelynmartins@gmail.com>
2021-02-02 17:19:58 -0300

    finalizar ex funcao

---
+++ vetor.js
@@ -0,0 +1,10 @@
+let signos = ["Gêmeos", "libra", "escorpião", "Leão", "câncer", "áries"]
+
+
+
+console.log(signos.length);
+
+for (let i = 0; i<=signos.length; i++) {
+
+    console.log(signos[i])
+}
